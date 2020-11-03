cf7632932147553cfbb4bd5137285fc57e0bb4c9
joshday <emailjoshday@gmail.com>
2020-11-02 19:30:08 -0500

    useable first version

--- src/Telperion.jl
+++ src/Telperion.jl
@@ -2,59 +2,63 @@ module Telperion
 
 using OrderedCollections: OrderedDict
 
-#-----------------------------------------------------------------------------# xy 
-function xy(df, ex::Expr)
-    (ex.args[1] === :~) && length(ex.args) == 3 || error("Formula must have the form `lhs ~ rhs`")
-    y = coldict(df, ex.args[2])
-    x = if ex.args[3] isa Expr
-        if ex.args[3].args[1] === :+
-            reduce(merge, coldict(df, a) for a in ex.args[3].args[2:end])
-        else
-            @warn "What are you even doing?" ex.args[3]
+export OrderedDict, @withprops, @xy, dummy
+
+#-----------------------------------------------------------------------------# @withprops
+macro withprops(df, ex)
+    esc(quote
+        eval(Telperion.replace_props!($df, $(Meta.quot(ex))))
+    end)
+end
+
+replace_props!(df, x) = x
+
+replace_props!(df, x::Symbol) = hasproperty(df, x) ? getproperty(df, x) : x
+
+function replace_props!(df, ex::Expr) 
+    Expr(ex.head, replace_props!.(Ref(df), ex.args)...)
+end
+
+#-----------------------------------------------------------------------------# @xy 
+macro xy(df, ex)
+    ex.args[1] === :~ && length(ex.args) == 3 || error("Formula must have the form `lhs ~ rhs`")
+    lhs = split_terms(ex.args[2])
+    rhs = split_terms(ex.args[3])
+
+    yblock = Expr(:call, :OrderedDict)
+    for term in lhs 
+        push!(yblock.args, :($(string(term)) => Telperion.@withprops($df, $term)))
+    end
+    xblock = Expr(:call, :OrderedDict)
+    for term in rhs 
+        push!(xblock.args, :($(string(term)) => Telperion.@withprops($df, $term)))
+    end
+    esc(quote
+        Telperion.process!($df, $xblock), Telperion.process!($df, $yblock)
+    end)
+end
+
+split_terms(x) = [x]
+function split_terms(x::Expr) 
+    x.head === :call && x.args[1] === :+ || error("Formula must have the form `y ~ 1 + x`")
+    x.args[2:end]
+end
+
+function process!(df, dict)
+    for (k,v) in dict 
+        if v isa OrderedDict 
+            for (k2, v2) in v 
+                dict["$k [$k2]"] = v2
+            end
+            delete!(dict, k)
+        elseif length(v) == 1 
+            dict[k] = fill(v, size(df, 1))
         end
-    else
-        coldict(df, ex.args[3])
     end
-    return x, y
+    dict
 end
 
-coldict(df, term) = OrderedDict(string(term) => calcterm(df, term))
-
-#-----------------------------------------------------------------------------# calcterm core
-calcterm(df, term::Number) = fill(term, size(df, 1))
-calcterm(df, term::Symbol) = getproperty(df, term)
-
-function calcterm(df, term::Expr) 
-    if term.head === :call 
-        f, args = term.args[1], term.args[2:end]
-        calcterm(df, Val(term.args[1]), term.args[2:end])
-    end
-end
-    
-
-#-----------------------------------------------------------------------------# Expr terms
-function calcterm(df, f::Val{T}, args) where {T} 
-    s = string(T)
-    @info s
-    @info args
-    # TODO: figure this out for broadcasted functions
-    if first(s) === '.' || last(s) === '.'
-        broadcast(eval(Meta.parse(replace(s, '.' => ""))), map(x -> calcterm(df, x), args)...)
-    else
-        eval(T)(map(x -> calc(df, x), args)...)
-    end
-end
-
-
-
-
-# # arbitrary function is applied elementwise
-# 
-
-# # special functions
-# calc(df, ::Val{:.+}, args) = sum(reduce(hcat, calc(df, a) for a in args), dims=2)
-
-# zscore(x) = (x - mean(x)) ./ std(x)
-# calc(df, ::Val{:zscore}, args) = zscore(map(x -> calc(df, x), args)...)
+#-----------------------------------------------------------------------------# functions 
+dummy(x; base=first) = OrderedDict("$l" => x .== l for l in sort(unique(x))[2:end])
 
 end
